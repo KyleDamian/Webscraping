d7beccff7d5dab27d95458f5b632d007a6697552
KyleDamian <khaddock@live.com>
2019-01-30 16:14:22 -0600

    move index.html file to templates folder

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ mongo = PyMongo(app)
 #  create route that renders index.html template
 @app.route("/")
 def index():
-    mars = mongo.db.mars_db.find_one()
-    return render_template("index.html", mars=mars)
+    mars_db = mongo.db.mars_db.find_one()
+    return render_template("index.html", mars_db=mars_db)
 
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars_db
+    mars_db = mongo.db.mars_db
     mars_data = scrape_mars.scrape()
-    mars.update({},mars_data,upsert=True)
+    mars_db.update({},mars_data,upsert=True)
 
     return redirect("/")
 
